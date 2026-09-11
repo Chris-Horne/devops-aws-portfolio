5e5277604e50f36601c1e9b86305c56c3dcedbd8
Chris Horne <chris@blklit.io>
2026-09-11 13:14:27 -0700

    Add force_delete option to ECR repository for automatic cleanup of images

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_ecr_repository" "portfolio_app_repo" {
   name                 = "devops-portfolio-app"
   image_tag_mutability = "MUTABLE"
-
+  force_delete         = true
   image_scanning_configuration {
     scan_on_push = true
   }
